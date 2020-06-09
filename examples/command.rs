13ad230a5bb53709d15daf445c9770bcddf5aa5c
Darren Mulholland <dmulholl@tcd.ie>
2020-06-09 23:18:04 +0100

    Add command example

---
+++ examples/command.rs
@@ -0,0 +1,20 @@
+extern crate arguably;
+use arguably::ArgParser;
+
+fn main() {
+    let mut parser = ArgParser::new()
+        .helptext("Usage: foobar...")
+        .version("1.0")
+        .command("boo", ArgParser::new()
+            .helptext("Usage: foobar boo...")
+            .callback(cmd_boo)
+        );
+
+    if let Err(err) = parser.parse() {
+        err.exit();
+    }
+}
+
+fn cmd_boo(_cmd: &str, _parser: &ArgParser) {
+    println!("boo!");
+}
